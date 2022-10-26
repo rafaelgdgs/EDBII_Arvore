e49b7c0b4d55a11a19e54aaa02f5870b1b20f47d
nusoto <rafagsique@hotmail.com>
2022-10-25 21:08:52 -0300

    implementation of 'enesimoElemento(int n)'

--- src/br/ufrn/imd/modelo/Tree.java
+++ src/br/ufrn/imd/modelo/Tree.java
@@ -9,7 +9,22 @@ public class Tree {
 	}
 	
 	public int enesimoElemento(int n) {
-		return 0;
+		if (n > (raiz.numNodeEsq + raiz.numNodeDir + 1) || n < 1) {
+			return -1;
+		}
+		Node current = raiz;
+		int pos = current.numNodeEsq + 1;
+		while(pos != n) {
+			if (n < pos) {
+				current = current.esq;
+				pos -= current.numNodeDir+1;
+			}
+			else {
+				current = current.dir;
+				pos += current.numNodeEsq+1;
+			}
+		}
+		return current.valor;
 	}
 	
 	public int posicao(int x) {
@@ -35,11 +50,12 @@ public class Tree {
 	public String pre_ordem() {
 		String res = new String();
 		res = pre_ordem_rec(raiz, res);
-		return res;
+		//return (res == null || res.isEmpty()) ? res : (res.substring(0, res.length() - 1));
+		return (res.endsWith(" ")) ? (res.substring(0, res.length() - 1)) : res;
 	}
 	
 	static String pre_ordem_rec(Node n, String s) {
-		s = s.concat(String.valueOf(n.valor) + " ");
+		s += String.valueOf(n.valor) + " ";
 		if (n.esq != null) {
 			s = pre_ordem_rec(n.esq, s);
 		}
@@ -48,7 +64,7 @@ public class Tree {
 		}
 		return s;
 	}
-	
+
 	public void imprimeArvore(int s) {
 		
 	}
--- src/br/ufrn/imd/visao/ABB.java
+++ src/br/ufrn/imd/visao/ABB.java
@@ -6,17 +6,38 @@ import br.ufrn.imd.modelo.Tree;
 public class ABB {
 
 	public static void main(String[] args) {
-		Tree t = new Tree(new Node(10));
-		t.raiz.esq = new Node(2);
-		t.raiz.esq.esq = new Node(3);
-		t.raiz.esq.esq.dir = new Node(4);
-		t.raiz.esq.dir = new Node(5);
-		t.raiz.dir = new Node(6);
-		t.raiz.dir.esq = new Node(7);
-		t.raiz.dir.dir = new Node(8);
-		t.raiz.dir.dir.esq = new Node(9);
+		Tree t = new Tree(new Node(8));
+		t.raiz.esq = new Node(3);
+		t.raiz.esq.esq = new Node(1);
+		t.raiz.esq.dir = new Node(6);
+		t.raiz.esq.dir.esq = new Node(4);
+		t.raiz.esq.dir.dir = new Node(7);
+		t.raiz.dir = new Node(10);
+		t.raiz.dir.dir = new Node(14);
+		t.raiz.dir.dir.esq = new Node(13);
+		
+		t.raiz.numNodeEsq = 5;
+		t.raiz.numNodeDir = 3;
+		t.raiz.esq.numNodeEsq = 1;
+		t.raiz.esq.numNodeDir = 3;
+		t.raiz.esq.esq.numNodeEsq = 0;
+		t.raiz.esq.esq.numNodeDir = 0;
+		t.raiz.esq.dir.numNodeEsq = 1;
+		t.raiz.esq.dir.numNodeDir = 1;
+		t.raiz.esq.dir.esq.numNodeEsq = 0;
+		t.raiz.esq.dir.esq.numNodeDir = 0;
+		t.raiz.esq.dir.dir.numNodeEsq = 0;
+		t.raiz.esq.dir.dir.numNodeDir = 0;
+		t.raiz.dir.numNodeEsq = 0;
+		t.raiz.dir.numNodeDir = 2;
+		t.raiz.dir.dir.numNodeEsq = 1;
+		t.raiz.dir.dir.numNodeDir = 0;
+		t.raiz.dir.dir.esq.numNodeEsq = 0;
+		t.raiz.dir.dir.esq.numNodeDir = 0;
 		
 		System.out.println(t.pre_ordem());
+		System.out.println(t.enesimoElemento(10));
+		
 	}
 
 }
