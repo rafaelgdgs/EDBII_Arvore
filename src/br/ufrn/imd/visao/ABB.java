275f058886512dcc7c6a164289ca82efbfaba378
nusoto <rafagsique@hotmail.com>
2022-11-08 08:00:14 -0300

    implemented read from file

--- src/br/ufrn/imd/visao/ABB.java
+++ src/br/ufrn/imd/visao/ABB.java
@@ -1,40 +1,89 @@
 package br.ufrn.imd.visao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
 import br.ufrn.imd.modelo.Node;
 import br.ufrn.imd.modelo.Tree;
 
 public class ABB {
+	
+	public static void lerDoArquivo(String arquivo1, String arquivo2) throws FileNotFoundException {
+        //File file = new File(arquivo1);
+        Scanner scanner1 = new Scanner(new FileReader(arquivo1));
+
+        List<Integer> vet = new ArrayList<Integer>();
+        while (scanner1.hasNext()) {
+        	vet.add(scanner1.nextInt());
+        }
+        Tree arvore = new Tree();
+        arvore.raiz = new Node(vet.get(0));
+        for(int l = 1; l < vet.size(); l++) {
+            arvore.inserir(vet.get(l));
+        }
+
+        Scanner scanner = new Scanner(new FileReader(arquivo2));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] textoLinha = line.split(" ");
+
+            if(Objects.equals(textoLinha[0], "INSIRA")) {
+                int valor = Integer.parseInt(textoLinha[1]);
+                if(arvore.inserir(valor))
+                    System.out.println(valor + " adicionado");
+                else System.out.println(valor + " já está na árvore, não pode ser inserido");
+            }
+            else if(Objects.equals(textoLinha[0], "BUSCAR")) {
+                if(arvore.buscar(Integer.parseInt(textoLinha[1])) != null)
+                    System.out.println("Chave encontrada");
+                else System.out.println("Chave não encontrada");
+            }
+            else if(Objects.equals(textoLinha[0], "REMOVA")) {
+                arvore.remover(Integer.parseInt(textoLinha[1]));
+            }
+            else if(Objects.equals(textoLinha[0], "ENESIMO")) {
+                System.out.println(arvore.enesimoElemento(Integer.parseInt(textoLinha[1])));
+            }
+            else if(Objects.equals(textoLinha[0], "POSICAO")) {
+                System.out.println(arvore.posicao(Integer.parseInt(textoLinha[1])));
+            }
+            else if(Objects.equals(textoLinha[0], "MEDIANA")) {
+                System.out.println(arvore.mediana());
+            }
+            else if(Objects.equals(textoLinha[0], "MEDIA")) {
+                System.out.println(arvore.media(Integer.parseInt(textoLinha[1])));
+            }
+            else if(Objects.equals(textoLinha[0], "CHEIA")) {
+                if (arvore.ehCheia()) {
+                	System.out.println("A arvore é cheia");
+                }
+                else {
+                	System.out.println("A arvore não é cheia");
+                }
+            }
+            else if(Objects.equals(textoLinha[0], "COMPLETA")) {
+                if (arvore.ehCompleta()) {
+                	System.out.println("A arvore é completa");
+                }
+                else {
+                	System.out.println("A arvore não é completa");
+                }
+            }
+            else if(Objects.equals(textoLinha[0], "PREORDEM")) {
+                System.out.println(arvore.pre_ordem());
+            }
+            else if(Objects.equals(textoLinha[0], "IMPRIMA")) {
+                arvore.imprimeArvore(Integer.parseInt(textoLinha[1]));
+            }
+        }
+    }
 
 	public static void main(String[] args) {
-//		Tree t = new Tree(new Node(8));
-//		t.raiz.esq = new Node(3);
-//		t.raiz.esq.esq = new Node(1);
-//		t.raiz.esq.dir = new Node(6);
-//		t.raiz.esq.dir.esq = new Node(4);
-//		t.raiz.esq.dir.dir = new Node(7);
-//		t.raiz.dir = new Node(10);
-//		t.raiz.dir.dir = new Node(14);
-//		t.raiz.dir.dir.esq = new Node(13);
-		
-//		t.raiz.numNodeEsq = 5;
-//		t.raiz.numNodeDir = 3;
-//		t.raiz.esq.numNodeEsq = 1;
-//		t.raiz.esq.numNodeDir = 3;
-//		t.raiz.esq.esq.numNodeEsq = 0;
-//		t.raiz.esq.esq.numNodeDir = 0;
-//		t.raiz.esq.dir.numNodeEsq = 1;
-//		t.raiz.esq.dir.numNodeDir = 1;
-//		t.raiz.esq.dir.esq.numNodeEsq = 0;
-//		t.raiz.esq.dir.esq.numNodeDir = 0;
-//		t.raiz.esq.dir.dir.numNodeEsq = 0;
-//		t.raiz.esq.dir.dir.numNodeDir = 0;
-//		t.raiz.dir.numNodeEsq = 0;
-//		t.raiz.dir.numNodeDir = 2;
-//		t.raiz.dir.dir.numNodeEsq = 1;
-//		t.raiz.dir.dir.numNodeDir = 0;
-//		t.raiz.dir.dir.esq.numNodeEsq = 0;
-//		t.raiz.dir.dir.esq.numNodeDir = 0;
-		
 		
 //		System.out.println("pre_ordem: " + t.pre_ordem());
 //		System.out.println("simetrica: " + t.simetrica());
@@ -47,7 +96,7 @@ public class ABB {
 //		System.out.println("mediana: " + t.mediana());
 //		System.out.println("ehCheia: " + (t.ehCheia() ? "True" : "False"));
 		
-		Tree t = new Tree();
+//		Tree t = new Tree();
 //		t.inserir(6);
 //		t.inserir(5);
 //		t.inserir(9);
@@ -57,15 +106,15 @@ public class ABB {
 //		t.inserir(7);
 //		t.inserir(3);
 		
-		t.inserir(5);
-		t.inserir(3);
-		t.inserir(2);
-		t.inserir(4);
-		t.inserir(8);
-		t.inserir(6);
-		t.inserir(9);
-		t.inserir(15);
-		t.inserir(1);
+//		t.inserir(5);
+//		t.inserir(3);
+//		t.inserir(2);
+//		t.inserir(4);
+//		t.inserir(8);
+//		t.inserir(6);
+//		t.inserir(9);
+//		t.inserir(15);
+//		t.inserir(1);
 		
 		
 		//System.out.println("raiz = " + t.raiz.valor);
@@ -73,12 +122,18 @@ public class ABB {
 		//System.out.println("enesimoElemento: " + t.enesimoElemento(4));
 		//Node c = t.buscar(5);
 		//System.out.println("Valor: " + c.valor + " ,  H: " + c.h + " , ih: " + c.ih);
-		t.imprimeArvore(1);
+//		t.imprimeArvore(1);
 		//t.imprimeArvore(2);
 		//System.out.println("ehCheia: " + (t.ehCheia() ? "True" : "False"));
 		//System.out.println("ehCompleta: " + (t.ehCompleta() ? "True" : "False"));
-		t.remover(5);
-		t.imprimeArvore(1);
+//		t.remover(5);
+//		t.imprimeArvore(1);
+		
+		try {
+			ABB.lerDoArquivo("\\\\wsl.localhost\\Ubuntu\\home\\jhinlinux\\EDBII\\tests\\arquivo1.txt", "\\\\wsl.localhost\\Ubuntu\\home\\jhinlinux\\EDBII\\tests\\arquivo2.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
